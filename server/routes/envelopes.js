0d8480b1d0ea94514e54794e1ad897b126b17810
david-alej <david12777@outlook.com>
2023-09-01 16:46:36 -0600

    started progress on route files

--- server/routes/envelopes.js
+++ server/routes/envelopes.js
@@ -1,2 +1,17 @@
 const express = require("express")
 const envelopesRouter = express.Router()
+
+const {
+  getAllEnvelopesFromDatabase,
+  getEnvelopeFromDatabaseById,
+  addEnvelopeToDatabase,
+  updateEnvelopeInDatabase,
+  deleteEnvelopeFromDatabasebyId,
+  deleteAllEnvelopesFromDatabase,
+} = require("../db/db")
+
+envelopesRouter.get("/", (req, res, next) => {
+  res.send()
+})
+
+module.exports = envelopesRouter
